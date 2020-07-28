c3fc07b73ca1d44633c371fcb3c2483d1ff491bf
Peter Shevchuk <raf02041994@gmail.com>
2020-07-28 13:25:28 +0300

    add

--- src/js/searchAndPlaginationHomePage.js
+++ src/js/searchAndPlaginationHomePage.js
@@ -1,23 +1,27 @@
-const pageUlr = 'https://image.tmdb.org/t/p/w500';
-const listImages = document.querySelector('.js-list');
-const notFound = 'https://blog.vverh.digital/wp-content/uploads/2020/06/oblojka-404.png';
-let src;
-let imgList = [];
+// const pageUlr = 'https://image.tmdb.org/t/p/w500';
+// const listImages = document.querySelector('.js-list');
+// const notFound = 'https://blog.vverh.digital/wp-content/uploads/2020/06/oblojka-404.png';
+// let src;
+// let imgList = [];
 
-let liArray = array.results.map(item => createlist(item)).join('');
+// let liArray = array.results.map(item => createlist(item)).join('');
 
-listImages.innerHTML = liArray;
+// listImages.innerHTML = liArray;
 
-function createlist(array) {
+// function createlist(array) {
 
-    if (array.poster_path !== null) {src = pageUlr+array.poster_path;} else {src = notFound;}
-    
-    let li = `<li class="list-items">
-        <img src="${src}" alt="" class="list-items__img">
-        <div class="layout">
-            <p class="list-items__title">${array.name}</p>
-        </div>   
-        </li>`
+//     if (array.poster_path !== null) {src = pageUlr+array.poster_path;} else {src = notFound;}
 
-    return li;
-}
+//     let li = `<li class="list-items">
+//         <img src="${src}" alt="" class="list-items__img">
+//         <div class="layout">
+//             <p class="list-items__title">${array.name}</p>
+//         </div>   
+//         </li>`
+
+//     return li;
+// }
+
+const btnSearch = document.querySelector('.search-film'); 
+
+
