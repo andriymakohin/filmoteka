ea60b5c78b6580cd9240dde4247c03a7ab0b3702
Peter Shevchuk <raf02041994@gmail.com>
2020-07-30 18:42:48 +0300

    edit others problem and change homepagejs on oop

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,22 +1,28 @@
-const pageUlr = 'https://image.tmdb.org/t/p/w500';
-const listImages = document.querySelector('.js-list');
-const notFound = '../images/404.png';
-let src = notFound;
-let title = "";
-function postList(array) {
-    let liArray = array.results.map(item => createList(item)).join('');
-    listImages.innerHTML = liArray;
+
+class loadList {
+    constructor() {
+        this.pageUlr = 'https://image.tmdb.org/t/p/w500';
+        this.listImages = document.querySelector('.js-list');
+        this.notFound = '../images/404.png';
+        this.src = this.notFound;
+        this.title = "";
+    }
+    postList(array) {
+        this.liArray = array.results.map(item => this.createList(item)).join('');
+        this.listImages.innerHTML = this.liArray;
+    }
+    createList(array) {
+        array.backdrop_path? this.src = this.pageUlr+array.backdrop_path : null;
+        array.poster_path? this.src = this.pageUlr+array.poster_path : null;
+        array.original_title? this.title = array.original_title : this.title = array.original_name;
+        
+        return `<li class="list-items" data-id="${array.id}">
+            <img src="${this.src}" alt="${this.title}" class="list-items__img">
+            <div class="layout">
+                <p class="list-items__title">${this.title}</p>
+            </div>   
+            </li>`;
+    }
 }
-function createList(array) {
-    array.backdrop_path? src = pageUlr+array.backdrop_path : null;
-    array.poster_path? src = pageUlr+array.poster_path : null;
-    array.original_title? title = array.original_title : title = array.original_name;
-    
-    let li = `<li class="list-items" data-id="${array.id}">
-        <img src="${src}" alt="${title}" class="list-items__img">
-        <div class="layout">
-            <p class="list-items__title">${title}</p>
-        </div>   
-        </li>`;
-    return li;
-}
+
+const loadPage = new loadList();
