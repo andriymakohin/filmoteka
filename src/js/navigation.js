99b2f5624844666fd976ccea9ba48323bcdf42ee
Andrii Makohin <andreas2010@ukr.net>
2020-07-30 22:51:17 +0300

    fixed Library

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -11,6 +11,7 @@ class navigation {
     this.formWrap = document.querySelector('.form-wrap');
     this.movieWrap = document.querySelector('.movies-wrap');
     this.list = document.querySelector('[data-page="home"]');
+    this.listLib = document.querySelector('[data-page="library"]');
     this.selectFilm = '';
   }
 
@@ -42,11 +43,13 @@ class navigation {
   init() {
     this.activeHomePage();
     this.list.addEventListener('click', () => this.activeDetailsPage());
+    this.listLib.addEventListener('click', () => this.activeDetailsPage());
     this.home_library[0].addEventListener('click', () => this.activeHomePage());
     this.home_library[1].addEventListener('click', () => this.activeLibraryPage());
     this.logo.addEventListener('click', () => this.activeHomePage());
     this.favoriteBtn.addEventListener('click', () => drawFilmList('filmsWatched'));
     this.queueBtn.addEventListener('click', () => drawFilmList('filmsQueue'));
+   
   }
 }
 
