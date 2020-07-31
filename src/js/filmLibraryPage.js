9d5813457d1c97cab6f6ffb251ba02bedb45d991
Peter Shevchuk <raf02041994@gmail.com>
2020-07-31 10:19:44 +0300

    fix search error

--- src/js/filmLibraryPage.js
+++ src/js/filmLibraryPage.js
@@ -5,6 +5,8 @@ class libraryPage {
   }
   drawFilmList(params) {
     this.movieMasyv = JSON.parse(localStorage.getItem('films'+params));
+    document.querySelector('.library__item--active')? document.querySelector('.library__item--active').classList.remove('library__item--active') : null;
+    if (params === 'Watched') {navigationPage.favoriteBtn.classList.add('library__item--active')} else {navigationPage.queueBtn.classList.add('library__item--active')}
     this.libraryList.innerHTML = 'You do not have to '+params+' movies to watch. Add them.';
     if (!this.movieMasyv) {return;}
     this.movieMasyv = this.movieMasyv.map(obj => this.createLibraryCardFunc(obj)).join('');
