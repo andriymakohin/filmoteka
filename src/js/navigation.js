c6d22a3429f96dc5007ebc35f01134a3f1bacac6
Andrii Makohin <andreas2010@ukr.net>
2020-08-02 05:16:56 +0300

    end

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -12,7 +12,6 @@ class Navigation {
     this.movieWrap = document.querySelector('.movies-wrap');
     this.list = document.querySelector('[data-page="home"]');
     this.listLib = document.querySelector('[data-page="library"]');
-    // this.scrolBtn = document.querySelector('.footer__btnSection');
     this.selectFilm = '';
   }
 
